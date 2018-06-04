774e08d49cebf8aafa1863604d3e5e9e7299986c
feixichen <250803841@qq.com>
2018-06-04 15:02:56 +0800

    天猫SSM项目 分类管理完成

--- src/main/java/com/how2java/tmall/mapper/CategoryMapper.java
+++ src/main/java/com/how2java/tmall/mapper/CategoryMapper.java
@@ -10,6 +10,14 @@ public interface CategoryMapper {
     public List<Category> list(Page page);
 
     public int total();
+
+    void add(Category category);
+
+    void delete(int id);
+
+    Category get(int id);
+
+    void update(Category category);
 }
 
 
--- src/main/java/com/how2java/tmall/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/how2java/tmall/service/impl/CategoryServiceImpl.java
@@ -23,4 +23,24 @@ public class CategoryServiceImpl implements CategoryService {
         return categoryMapper.total();
     }
 
+    @Override
+    public void add(Category category) {
+        categoryMapper.add(category);
+    }
+
+    @Override
+    public void delete(int id) {
+        categoryMapper.delete(id);
+    }
+
+    @Override
+    public Category get(int id) {
+        return categoryMapper.get(id);
+    }
+
+    @Override
+    public void update(Category category) {
+        categoryMapper.update(category);
+    }
+
 }
