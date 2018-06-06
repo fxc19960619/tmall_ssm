c0fd56579552fb9570b01837c8067d6bcdf95071
feixichen <250803841@qq.com>
2018-06-06 19:45:42 +0800

    天猫SSM项目 后台管理完成

---
+++ src/main/java/com/how2java/tmall/service/impl/OrderServiceImpl.java
@@ -0,0 +1,59 @@
+package com.how2java.tmall.service.impl;
+
+import com.how2java.tmall.mapper.OrderMapper;
+import com.how2java.tmall.pojo.Order;
+import com.how2java.tmall.pojo.OrderExample;
+import com.how2java.tmall.pojo.User;
+import com.how2java.tmall.service.OrderService;
+import com.how2java.tmall.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class OrderServiceImpl implements OrderService {
+    @Autowired
+    OrderMapper orderMapper;
+    @Autowired
+    UserService userService;
+
+    @Override
+    public void add(Order c) {
+        orderMapper.insert(c);
+    }
+
+    @Override
+    public void delete(int id) {
+        orderMapper.deleteByPrimaryKey(id);
+    }
+
+    @Override
+    public void update(Order c) {
+        orderMapper.updateByPrimaryKeySelective(c);
+    }
+
+    @Override
+    public Order get(int id) {
+        return orderMapper.selectByPrimaryKey(id);
+    }
+
+    @Override
+    public List<Order> list() {
+        OrderExample orderExample=new OrderExample();
+        orderExample.setOrderByClause("id desc");
+        List<Order> result=orderMapper.selectByExample(orderExample);
+        setUser(result);
+        return result;
+    }
+
+    private void setUser(List<Order> os) {
+        for (Order o:os){
+            setUser(o);
+        }
+    }
+    private  void  setUser(Order o){
+        int uid=o.getUid();
+        User u =userService.get(uid);
+        o.setUser(u);
+    }
+}
