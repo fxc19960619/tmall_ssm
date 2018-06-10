79d91fe5c335e78fe85318a3e903e6bacbda36ae
feixichen <250803841@qq.com>
2018-06-10 21:31:19 +0800

    天猫SSM项目 基本完成

--- src/main/java/com/how2java/tmall/service/impl/OrderItemServiceImpl.java
+++ src/main/java/com/how2java/tmall/service/impl/OrderItemServiceImpl.java
@@ -56,6 +56,28 @@ public class OrderItemServiceImpl implements OrderItemService {
         }
     }
 
+    @Override
+    public int getSaleCount(int pid) {
+        OrderItemExample example =new OrderItemExample();
+        example.createCriteria().andPidEqualTo(pid);
+        List<OrderItem> ois =orderItemMapper.selectByExample(example);
+        int result =0;
+        for (OrderItem oi : ois) {
+            result+=oi.getNumber();
+        }
+        return result;
+    }
+
+
+    @Override
+    public List<OrderItem> listByUser(int uid) {
+        OrderItemExample example =new OrderItemExample();
+        example.createCriteria().andUidEqualTo(uid).andOidIsNull();
+        List<OrderItem> result = orderItemMapper.selectByExample(example);
+        setProduct(result);
+        return result;
+    }
+
     public void fill(Order o) {
         OrderItemExample example =new OrderItemExample();
         example.createCriteria().andOidEqualTo(o.getId());
@@ -72,28 +94,6 @@ public class OrderItemServiceImpl implements OrderItemService {
         o.setTotal(total);
         o.setTotalNumber(totalNumber);
         o.setOrderItems(ois);
-
-    }
-
-    @Override
-    public int getSaleCount(int pid) {
-        OrderItemExample example =new OrderItemExample();
-        example.createCriteria().andPidEqualTo(pid);
-        List<OrderItem> ois =orderItemMapper.selectByExample(example);
-        int result =0;
-        for (OrderItem oi : ois) {
-            result+=oi.getNumber();
-        }
-        return result;
-    }
-
-    @Override
-    public List<OrderItem> listByUser(int uid) {
-        OrderItemExample example =new OrderItemExample();
-        example.createCriteria().andIdEqualTo(uid).andOidIsNull();
-        List<OrderItem> result = orderItemMapper.selectByExample(example);
-        setProduct(result);
-        return result;
     }
 
     public void setProduct(List<OrderItem> ois){
@@ -106,7 +106,5 @@ public class OrderItemServiceImpl implements OrderItemService {
         Product p = productService.get(oi.getPid());
         oi.setProduct(p);
     }
-
-    ;
 
 }
---
+++ src/main/webapp/WEB-INF/jsp/include/fore/cart/orderConfirmedPage.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 25080
+  Date: 2018/6/10
+  Time: 21:28
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+         pageEncoding="UTF-8" isELIgnored="false"%>
+
+<div class="orderFinishDiv">
+    <div class="orderFinishTextDiv">
+        <img src="img/site/orderFinish.png">
+        <span>交易已经成功，卖家将收到您的货款。</span>
+    </div>
+</div>
